02e2ceb71ec9a35ad9fe3d47d810a81140c861d3
chrisez908 <cez16@txstate.edu>
2022-09-21 11:41:05 -0500

    completeed task 3 and 4

--- javasscript/external.js
+++ javasscript/external.js
@@ -2,5 +2,19 @@
 //document.write("Where is my JS code?")
 //window.alert(5+6)
 //window.alert('5+6')
-document.write('<h1>A heading</h1>');
-document.write('<p>A sentence.</p>')
+//document.write('<h1>A heading</h1>');
+//document.write('<p>A sentence.</p>')
+/*const x="web";
+const y = "mapping";
+const out = x+y
+document.write(out);*/
+//A prompt box is used to prompt users to input a value before entering a page.
+/*user_name = window.prompt("Please enter your name","Type your name here");
+document.write(user_name);*/
+x=22;
+const y = 33;
+document.writeln(x+y);
+document.writeln("<br>");
+document.writeln(x+=y);
+document.writeln("<br>");
+document.writeln(x+=y);
