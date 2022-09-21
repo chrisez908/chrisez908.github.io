112e2c6b442db0f1145f089d3d76143a561518b8
chrisez908 <cez16@txstate.edu>
2022-09-21 11:49:04 -0500

    Task 5 conditional Statement

--- javasscript/external.js
+++ javasscript/external.js
@@ -11,10 +11,23 @@ document.write(out);*/
 //A prompt box is used to prompt users to input a value before entering a page.
 /*user_name = window.prompt("Please enter your name","Type your name here");
 document.write(user_name);*/
-x=22;
-const y = 33;
+/*const y = 33;
 document.writeln(x+y);
 document.writeln("<br>");
 document.writeln(x+=y);
 document.writeln("<br>");
-document.writeln(x+=y);
+document.writeln(x+=y);*/
+document.writeln("<button onclick='condition()'>Conditional Test</button>")
+function condition()
+{
+  x=confirm("are you sure you want to proceed?");
+  if(x)
+{
+  document.writeln("You chose Okay!");
+}
+else
+{
+document.writeln("You chose Cancel!");  
+}
+
+}
