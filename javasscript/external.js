0be3c211a32cc30a52790873162f717d9dd95f60
chrisez908 <cez16@txstate.edu>
2022-09-22 19:53:26 -0500

    Executed the welcome function in external.js from an internal script.

--- javasscript/external.js
+++ javasscript/external.js
@@ -42,9 +42,8 @@ function welcome()
   let a = "Please enter your name.";
   let b = "Type your name here.";
   // A prompt box is used to prompt users to input a value before entering a page.
-  user_name=window.prompt(a,b);
-  message="<h1>Hello, welcome to my webpage, "+ user_name+ "!</h1>"
+  user_name = window.prompt(a,b);
+  message = "<h1>Hello, welcome to my webpage, "+ user_name + "!</h1>"
   return message
 }
-document.write(welcome());
 document.write(message);
