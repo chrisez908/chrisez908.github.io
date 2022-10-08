a0926052fc96da38cb21a92ad7e8cdd76ff036ab
chrisez908 <cez16@txstate.edu>
2022-10-08 16:03:31 -0500

    FIXED EVERYTHING

--- javasscript/map.js
+++ javasscript/map.js
@@ -1,48 +1,53 @@
-var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+var mymap = L.map('mapid').setView([0, 0], 1);
 // var mymap = L.map('mapid');
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {}).addTo(mymap);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+}).addTo(mymap);
+
+var Great = L.marker([40.33,116.48]).addTo(mymap);
+
 var Taj = L.marker([27.17, 78.04]).addTo(mymap);
-  marker.bindPopup("India's Taj Mahal").openPopup();
-  marker.on('click', function(e)
-  {
-    mymap.setView(e.latlng, 14);
-  })
-  var Great_Wall = L.marker([40.33, 116.48]).addTo(mymap);
-    marker.bindPopup("Great Wall China").openPopup();
-    marker.on('click', function(e)
-    {
-      mymap.setView(e.latlng, 14);
-    })
-    var Petra_In_Jordan = L.marker([30.32, 35.44]).addTo(mymap);
-       marker.bindPopup("Petra in Jordan").openPopup();
-       marker.on('click', function(e)
-       {
-         mymap.setView(e.latlng, 14);
-       })
-        var BrazilChrist = L.marker([-22.95, -43.21]).addTo(mymap);
-          marker.bindPopup("Brazil's Statue of Christ the Redeemer").openPopup();
-          marker.on('click', function(e)
-          {
-            mymap.setView(e.latlng, 14);
-          })
-          var MacchuPichu = L.marker([-13.16, -72.55]).addTo(mymap);
-            marker.bindPopup("Peru's Macchu Picchu").openPopup();
-            marker.on('click', function(e)
-            {
-              mymap.setView(e.latlng, 14);
-            })
+  // marker.bindPopup("India's Taj Mahal").openPopup();
+  // marker.on('click', function(e)
+  // {
+  //   mymap.setView(e.latlng, 14);
+  // })
+  var Petra = L.marker([30.32, 35.44]).addTo(mymap);
+  var Brazil = L.marker([-22.95,-43.21]).addTo(mymap);
+  var Macchu=L.marker([-13.16,-72.55]).addTo(mymap);
+  // var marker = L.marker([40.33, 116.48]).addTo(mymap);
+  //     marker.on('click', function(e){
+  //     mymap.setView(e.latlng, 14);
+  //     })
+    // var Petra_In_Jordan = L.marker([30.32, 35.44]).addTo(mymap);
+       // marker.bindPopup("Petra in Jordan").openPopup();
+       // marker.on('click', function(e)
+       // {
+       //   mymap.setView(e.latlng, 14);
+       // })
+        // var BrazilChrist = L.marker([-22.95, -43.21]).addTo(mymap);
+        //   marker.bindPopup("Brazil's Statue of Christ the Redeemer").openPopup();
+        //   marker.on('click', function(e)
+        //   {
+        //     mymap.setView(e.latlng, 14);
+        //   })
+          // var MacchuPichu = L.marker([-13.16, -72.55]).addTo(mymap);
+          //   marker.bindPopup("Peru's Macchu Picchu").openPopup();
+          //   marker.on('click', function(e)
+          //   {
+          //     mymap.setView(e.latlng, 14);
+          //   })
             var ChichenItza = L.marker([20.68, -88.57]).addTo(mymap);
-              marker.bindPopup("Chichen Itza Pyramid").openPopup();
-              marker.on('click', function(e)
-              {
-                mymap.setView(e.latlng, 14);
-              })
+              // marker.bindPopup("Chichen Itza Pyramid").openPopup();
+              // marker.on('click', function(e)
+              // {
+              //   mymap.setView(e.latlng, 14);
+              // })
               var Colosseum = L.marker([41.89, 12.49]).addTo(mymap);
-                marker.bindPopup("The Colosseum in Rome").openPopup();
-                marker.on('click', function(e)
-                {
-                  mymap.setView(e.latlng, 14);
-                })
+                // marker.bindPopup("The Colosseum in Rome").openPopup();
+                // marker.on('click', function(e)
+                // {
+                //   mymap.setView(e.latlng, 14);
+                // })
 var circle = L.circle([51.508, -0.11],
   {
     color: 'red',
