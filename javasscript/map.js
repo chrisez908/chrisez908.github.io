90c3f938f7595680f5d04fad40fcbb7f0a5136e1
chrisez908 <cez16@txstate.edu>
2022-10-08 15:12:02 -0500

    test

--- javasscript/map.js
+++ javasscript/map.js
@@ -1,7 +1,7 @@
 // var mymap = L.map('mapid').setView([51.505, -0.09], 13);
-// var mymap = L.map('mapid');
-// L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-// }).addTo(mymap);
+var mymap = L.map('mapid');
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+}).addTo(mymap);
 
 var Taj = L.marker([27.17, 78.04]).addTo(mymap);
   marker.bindPopup("India's Taj Mahal").openPopup();
